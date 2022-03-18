f611d1e1da1eb65de8d5a6c59f410cdda762caf6
yjourlin <yann.jourlin@gmail.com>
2022-03-18 17:29:07 +0100

    Import requests

---
+++ adquat_advanced/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+############################################################################################
+#
+#    ADQUAT - AJOUTE UN CHAMP DATE POUR CHOISIR LA DATE A LAQUELLE A ETE REALISE L'INVENTAIRE
+#
+############################################################################################
+
+
+{
+    'name': 'Adquat Advanced',
+    'version': '0.2',
+    'category': 'Tools',
+    'description': """
+    Ajouts divers
+""",
+    'author': 'Adquat-solutions',
+    'website': 'http://www.adquat.com',
+    'depends': ['base','product','stock','sale','sale_project'],
+    'data': [],
+    'demo': [],
+    'update_xml': [
+        #'security/ir.model.access.csv',
+        #'models_view.xml',
+    ],
+    'test':[],
+    'installable': True,
+    'images': [],
+}
---
+++ adquat_advanced/models.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+# Part of Odoo. See LICENSE file for full copyright and licensing details.
+from werkzeug import urls
+
+from odoo import api, fields, models
+import requests
+
+class ServerAction(models.Model):
+    """ Add website option in server actions. """
+
+    _name = 'ir.actions.server'
+    _inherit = 'ir.actions.server'
+
+    @api.model
+    def _get_eval_context(self, action):
+        """ Override to add the request object in eval_context. """
+        eval_context = super(ServerAction, self)._get_eval_context(action)
+        if action.state == 'code':
+            eval_context['requests'] = requests
+
+        return eval_context
